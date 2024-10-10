f4f347fcbc80cac96fe238b8bd37c15750f9249e
Fabix1204 <fabialeitner@tsn.at>
2024-10-10 14:47:30 +0200

    deletet comments

--- Aufgabe_02/Aufgabe_02.py
+++ Aufgabe_02/Aufgabe_02.py
@@ -17,32 +17,32 @@ temp2010 = temp[year == 2010]
 
 #1.1
 
-# plt.boxplot([temp1980, temp1990, temp2000, temp2010])
-# plt.show()
+plt.boxplot([temp1980, temp1990, temp2000, temp2010])
+plt.show()
 
 #1.2
 
-# plt.scatter(day[year == 2010], temp2010)
-# plt.show()
+plt.scatter(day[year == 2010], temp2010)
+plt.show()
 
 #1.3
 
-# low1980 = np.quantile(temp1980, 0.05)
-# high1980 = np.quantile(temp1980, 0.95)
-# low2010 = np.quantile(temp2010, 0.05)
-# high2010 = np.quantile(temp2010, 0.95)
+low1980 = np.quantile(temp1980, 0.05)
+high1980 = np.quantile(temp1980, 0.95)
+low2010 = np.quantile(temp2010, 0.05)
+high2010 = np.quantile(temp2010, 0.95)
 
-# print("1980: ", low1980, high1980)
-# print("2020: ", low2010, high2010)
+print("1980: ", low1980, high1980)
+print("2020: ", low2010, high2010)
 
 #1.4
 
-# for i in range(2010, 2021):
-#     temp_year = temp[year == i]
-#     median = np.median(temp_year)
-#     plt.bar(i, median, color='blue')
+for i in range(2010, 2021):
+    temp_year = temp[year == i]
+    median = np.median(temp_year)
+    plt.bar(i, median, color='blue')
     
-# plt.show()
+plt.show()
 
 #1.5
 
