82c9cce6679633505de9a317e38b332a00bcb3e9
Fabix1204 <fabialeitner@tsn.at>
2024-10-14 08:23:23 +0200

    updated Aufgabe_02

--- Aufgabe_02/Aufgabe_02.py
+++ Aufgabe_02/Aufgabe_02.py
@@ -15,17 +15,23 @@ temp1990 = temp[year == 1990]
 temp2000 = temp[year == 2000]
 temp2010 = temp[year == 2010]
 
-#1.1
+# 1.1
+# Interpretation: Die Temperatur hat sich über die Jahre erhöht.
+# Die Temperaturen liegen zwischen 5°C und 16°C
 
 plt.boxplot([temp1980, temp1990, temp2000, temp2010])
 plt.show()
 
-#1.2
+# 1.2
+# Interpretation: Im Sommer ist es deutlich wärmer als im Winter
+# Die meisten Werte liegen zwischen 0°C und 20°C
+
 
 plt.scatter(day[year == 2010], temp2010)
 plt.show()
 
-#1.3
+# 1.3
+# Interpretation: Die 95% Quantile haben sich zwischen 1980 und 2010 erhöht
 
 low1980 = np.quantile(temp1980, 0.05)
 high1980 = np.quantile(temp1980, 0.95)
@@ -35,7 +41,10 @@ high2010 = np.quantile(temp2010, 0.95)
 print("1980: ", low1980, high1980)
 print("2020: ", low2010, high2010)
 
-#1.4
+# 1.4
+# Interpetation: 2011 war die Durchsnittstemperatur im Vergleich zu
+# den umliegenden Jahren deutlich höher. 2016 dagegen war die Durchschnittstemperatur
+# deutlich niedriger als in den umliegenden Jahren
 
 for i in range(2010, 2021):
     temp_year = temp[year == i]
@@ -44,7 +53,9 @@ for i in range(2010, 2021):
     
 plt.show()
 
-#1.5
+# 1.5
+# Interpretation: Im Sommer ist es deutlich wärmer als im Winter
+# August ist der wärmste Monat
 
 for i in range(1, 13):
     temp_month = temp[(year == 2000) & (month == i)]
